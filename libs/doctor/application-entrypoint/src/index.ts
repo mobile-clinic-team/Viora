82ce2dfddec0e2a84568f62a3613acc63f07c850
Đoàn Quốc Việt <doanquocviet30062006@gmail.com>
2026-09-04 16:03:07 +0700

    feat(DOC-001): establish doctor domain application boundary

---
+++ libs/doctor/application-entrypoint/src/index.ts
@@ -0,0 +1,8 @@
+export * from '../../application/src/index.ts';
+export * from '../../contracts/src/index.ts';
+export * from '../../domain/src/index.ts';
+export type {
+  DepartmentRepository,
+  DoctorRepository,
+  ShiftRepository,
+} from '../../data-access/src/index.ts';
